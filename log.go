22baa42a9d53b2f04d7b5398fb125934864da22a
solon <solon.neto@open-co.com>
2024-06-04 16:50:11 -0300

    adding scroll to log

--- log.go
+++ log.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	tea "github.com/charmbracelet/bubbletea"
+	"strings"
 )
 
 type log struct {
@@ -9,6 +10,8 @@ type log struct {
 	log     string
 	logChan chan string
 	sub     chan string
+	buffer  []string
+	index   int
 }
 
 type logEvents struct {
@@ -32,14 +35,31 @@ func UpdateLog(m model, msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.log = log{sub: make(chan string)}
 			m.currentView = "pod"
 			return m, nil
+		case "up": // scroll up
+			if m.log.index > 0 {
+				m.log.index--
+			}
+		case "down": // scroll down
+			if m.log.index < len(m.log.buffer)-1 {
+				m.log.index++
+			}
 		}
 	case logEvents:
 		m.log.log += msg.event
+		m.log.buffer = append(m.log.buffer, msg.event)
 		return m, waitForLogActivity(m.log.sub)
 	}
 	return m, nil
 }
 
 func logView(m model) string {
-	return m.log.log
+	if len(m.log.buffer) == 0 {
+		return ""
+	}
+	start := m.log.index
+	end := start + 50
+	if end > len(m.log.buffer) {
+		end = len(m.log.buffer)
+	}
+	return strings.Join(m.log.buffer[start:end], "\n")
 }
